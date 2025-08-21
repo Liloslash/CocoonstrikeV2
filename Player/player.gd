bd91e48bbf863f9a7dfad19d67a6660a3054e8ff
Liloslash <loicdupuis@live.fr>
2025-08-21 21:19:29 +0200

    Enhance player input handling by refining mouse motion rotation and adjusting input settings

--- Player/player.gd
+++ Player/player.gd
@@ -12,6 +12,11 @@ var acceleration_duration: float = 0.5  # 0.5 seconde pour atteindre max_speed
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("esc"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * 0.002)
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Appliquer la gravité
